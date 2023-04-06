a5150c557210bb96c87698110908e6cf3b634007
Laura Cimpoesu <lauracsbusiness@gmail.com>
2023-04-06 16:51:17 +0200

    optimisation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll(".card")
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
-    removeActiveClass();
-    card.classList.add("active");
-  });
-});
+    removeActiveClass()
+    card.classList.add("active")
+  })
+})
 
 function removeActiveClass() {
   cards.forEach((card) => {
-    card.classList.remove("active");
-  });
+    card.classList.remove("active")
+  })
 }
